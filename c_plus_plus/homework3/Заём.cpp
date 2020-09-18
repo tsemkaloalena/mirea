651823f3bc2fd47bc01dd205361b4659a9ac5665
КАФИЁЧЕК <49063241+tsemkaloalena@users.noreply.github.com>
2020-09-18 21:37:46 +0300

    Update Заём.cpp

--- c_plus_plus/homework3/Заём.cpp
+++ c_plus_plus/homework3/Заём.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
 	setlocale(0, "");
-	float S, p, n, m, r;
+	float S, p, m, r;
+	int n;
 	cout << "Введите значение S" << endl;
 	cin >> S;
 	cout << "Введите значение p" << endl;
@@ -12,6 +13,10 @@ int main() {
 	r = p / 100;
 	cout << "Введите значение n" << endl;
 	cin >> n;
-	m = S * r * pow(1 + r, n) / (12 * (pow(1 + r, n) - 1));
-	cout << "Месячная выплата равна " << m << endl;
+	if (n == 0 or r == 0 or (r == -2 and n % 2 == 0)) {
+		cout << "Не могу посчитать" << endl;
+	}
+	else {
+		m = S * r * pow(1 + r, n) / (12 * (pow(1 + r, n) - 1));
+		cout << "Месячная выплата равна " << m << endl;
 }
