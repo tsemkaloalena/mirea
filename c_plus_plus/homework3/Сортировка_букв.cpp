2bd014736f21772a4d89ec1af582a5c0a1a8d5c4
КАФИЁЧЕК <49063241+tsemkaloalena@users.noreply.github.com>
2020-09-26 13:48:24 +0300

    Усложнённая

--- c_plus_plus/homework3/Сортировка_букв.cpp
+++ c_plus_plus/homework3/Сортировка_букв.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+
 #include <algorithm>
+
 using namespace std;
 
+
+
 int main() {
-	string str = "ntkdhnlprebaghcprhkzakbdprjscm";
-	char list[30];
+	string str;
+	cin >> str;
+	int n = str.size();
+
+	char *list = new char[n];
+
 	int i;
-	for (i = 0; i < 30; i++) {
+
+	for (i = 0; i < n; i++) {
 		list[i] = str[i];
 	}
-	sort(list, list + 30);
-	for (i = 0; i < 30; i++) {
+
+	sort(list, list + n);
+
+	for (i = 0; i < n; i++) {
 		cout << list[i];
 	}
+
 	return 0;
+
 }
