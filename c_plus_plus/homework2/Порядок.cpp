2c8087e5e0eb1f286b0a6f58aa785da35f674328
КАФИЁЧЕК <49063241+tsemkaloalena@users.noreply.github.com>
2020-09-26 13:04:27 +0300

    Усложнённая

--- c_plus_plus/homework2/Порядок.cpp
+++ c_plus_plus/homework2/Порядок.cpp
@@ -3,14 +3,33 @@ using namespace std;
 
 int main() {
 	setlocale(0, "");
-	int n, i;
+	int n, i, k;
+	bool f;
+
+	cout << "Введите первое значение" << endl;
 	cin >> n;
+
+	cout << "Введите кол-во значений" << endl;
+	cin >> k;
+
+	cout << "Последовательность возрастает?" << endl;
+	cin >> f;
+
+
 	if (n < 0) {
 		cout << n << " - не натуральное число";
 	}
+
 	else {
-		for (i = 0; i < 10; i++) {
-			cout << i + 1 << ") " << n + i << endl;
+			for (i = 0; i < k; i++) {
+				if (f) {
+					cout << i + 1 << ") " << n + i << endl;
+				}
+				else {
+					cout << i + 1 << ") " << n - i << endl;
+				}
+			}
 		}
-	}
+
+
 }
