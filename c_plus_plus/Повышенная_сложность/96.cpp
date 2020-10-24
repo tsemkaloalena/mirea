bdc77bc5152167d043e43c4895bf9bbe704937d2
КАФИЁЧЕК <49063241+tsemkaloalena@users.noreply.github.com>
2020-10-24 12:57:33 +0300

    Update 96.cpp

--- c_plus_plus/Повышенная_сложность/96.cpp
+++ c_plus_plus/Повышенная_сложность/96.cpp
@@ -5,31 +5,35 @@
 using namespace std;
 
 int main() {
+	setlocale(0, "");
 	int minus = 0, plus = 0;
-	int m = 37, i = 1, c = 10, n = 3, s = 5, j, k;
+	int m = 37, i = 1, c = 42, n = 3, s = 5, j, k;
 	int x[4], y[4], yy;
 
 	//Генерируем 4 числа
-	x[0] = (m * s + i) % c;
+	x[0] = (m * s + i) % c % 10;
 	i++;
-	x[1] = (m * s + i) % c;
+	x[1] = (m * s + i) % c % 10;
 	i++;
 	while (x[1] == x[0]) {
-		x[1] = (m * s + i) % c;
+		x[1] = (m * s + i) % c % 10;
 		i++;
 	}
-	x[2] = (m * s + i) % c;
+	x[2] = (m * s + i) % c % 10;
 	i++;
 	while (x[2] == x[0] or x[2] == x[1]) {
-		x[2] = (m * s + i) % c;
+		x[2] = (m * s + i) % c % 10;
 		i++;
 	}
-	x[3] = (m * s + i) % c;
+	x[3] = (m * s + i) % c % 10;
 	i++;
 	while (x[3] == x[0] or x[3] == x[1] or x[3] == x[2]) {
-		x[3] = (m * s + i) % c;
+		x[3] = (m * s + i) % c % 10;
 		i++;
 	}
+
+	//cout << x[0] << " " << x[1] << " " << x[2] << " " << x[3] << endl;
+
 
 	while (plus != 4) {
 		minus = 0;
@@ -57,10 +61,11 @@ int main() {
 			}
 		}
 
-		cout << x[0] << x[1] << x[2] << x[3] << endl;
-		cout << y[0] << y[1] << y[2] << y[3] << endl;
-		cout << plus << endl;
-		cout << minus << endl;
+		
+		//cout << y[0] << " " << y[1] << " " << y[2] << " " << y[3] << endl;
+		cout << "Плюсов: " << plus << endl;
+		cout << "Минусов: " << minus << endl;
+		cout << endl;
 	}
 
 	return 0;
